92359d942c476a6c0ee7e41a858b19d432eb3c56
ALEXANDER PLETZER <axp5530@psu.edu>
2015-11-29 19:24:06 -0700

    cleaner code

--- csvTableFromHtml.py
+++ csvTableFromHtml.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
+
 import urllib2
 from bs4 import BeautifulSoup
 import re
@@ -8,44 +10,50 @@ class CsvTableFromHtml:
 
     def __init__(self, url):
         self.soup = BeautifulSoup(urllib2.urlopen(url).read(), 'html.parser')
+        self.tables = []
         
     def findTables(self, className="wikitable sortable"):
-        self.tables = self.soup.find_all('table', attrs={"class": className})
+        tables = self.soup.find_all('table', attrs={"class": className})
+        for table in tables:
+            dataset = []
+            # get the headers
+            headers = [th.get_text() for th in table.find("tr").find_all("th")]
+            if headers:
+        	    dataset.append(headers)
+            # get the data 
+            for row in table.find_all("tr")[1:]:
+                datarow = [td.get_text() for td in row.find_all("td")]
+                if datarow:
+            	    dataset.append(datarow)
+            self.tables.append(dataset)
         
     def getNumberOfTables(self):
         return len(self.tables)
         
-    def getTable(self, index):
-        return self.tables[index]
-        
-    def convertToCSV(self, index):
-        res = ''
+    def printTable(self, index):
         for row in self.tables[index]:
-            for col in row:
-                val = self.__filter(repr(col))
-                res += val + ','
-            res +='\n'
-        return res
+            line = ''
+            for val in row:
+                line += val + ','
+            print(re.sub(r'\,\s*$', '', re.sub(r'\,\s*\,', ',NA,', line)))
         
-    def __filter(self, line):
-    	newLine = line.decode('UTF-8')
-    	for s in r'\<td[^\>]*\>', r'\<a[^\>]*\>', r'\<\/a\>', \
-                 r'\<\/td\>', r'\\n', r"u''", \
-                 r'\<th[^\>]*\>', r'\<\/th\>', r'\<br\>':
-        	newLine = re.sub(s, '', newLine)
-    	return newLine
-            
+    def saveTableToCSV(self, filename, index):
+        f = open(filename, 'w')
+        for row in self.tables[index]:
+            line = ''
+            for val in row:
+                line += val + ','
+            line = re.sub(r'\,\s*$', '', re.sub(r'\,\s*\,', ',NA,', line))
+            print(line.encode('utf-8'), file=f)
         
 #############################################################################
 def test():
     url = 'https://en.wikipedia.org/wiki/List_of_countries_by_intentional_homicide_rate'
     page = CsvTableFromHtml(url)
     page.findTables()
-    for tableIndex in range(page.getNumberOfTables()):
-    	print 'tableIndex = ', tableIndex
-        csvTable = page.convertToCSV(tableIndex)
-    	f = open('table{}.csv'.format(tableIndex), 'w')
-    	f.write(csvTable.encode('utf-8'))
+    page.printTable(2)
+    for ti in range(page.getNumberOfTables()):
+        page.saveTableToCSV('table{}.csv'.format(ti), 2)
     
 if __name__ == '__main__': test()
         
